88e643e4487e4680f22c23b63b00b49f189efe47
Cacia Rodrigues <caciarodriguues@gmail.com>
2023-06-16 16:53:07 -0300

    Mudanças finais + código das estastisticas

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const controle = document.querySelectorAll('[data-controle]');
+const estatisticas = document.querySelectorAll('[data-estatistica]');
 
 const pecas = {
   "bracos": {
@@ -37,6 +38,7 @@ const pecas = {
 controle.forEach((elemento) => {
   elemento.addEventListener('click', (evento) => {
     manipulaDados(evento.target.dataset.controle, evento.target.parentNode);
+    atualizaEstatisticas(evento.target.dataset.peca);
   });
 });
 
@@ -48,4 +50,10 @@ function manipulaDados(operacao, controle) {
   } else {
     peca.value = parseInt(peca.value) + 1;
   }
+}
+
+function atualizaEstatisticas(peca) {
+   estatisticas.forEach((elemento) => {
+     elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica]
+  })
 }
